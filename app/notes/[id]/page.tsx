f73b640c5230b42004bb035009697c9db3f1ac94
Kostiantyn Chesak <K03.chesak@gmail.com>
2025-07-19 01:26:26 +0200

    Type '{ params: { id: string; }; }' does not satisfy the constraint 'PageProps'.

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -2,24 +2,22 @@
 import NoteDetailsClient from "./NoteDetails.client";
 import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
 import { fetchNoteById } from "../../../lib/api";
+import { JSX } from "react/jsx-runtime";
 
 
-export default async function NoteDetailsPage({
+export default (async function NoteDetailsPage({
   params,
 }: {
   params: { id: string };
 }) {
-  const { id } = params;
   const queryClient = new QueryClient();
-
   await queryClient.prefetchQuery({
-    queryKey: ["note", id], 
-    queryFn: () => fetchNoteById(id),  
+    queryKey: ['note', params.id],
+    queryFn: () => fetchNoteById(params.id),
   });
-
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
       <NoteDetailsClient />
     </HydrationBoundary>
   );
-}
+}) as unknown as (props: { params: { id: string } }) => Promise<JSX.Element>;
--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -1,29 +1,24 @@
 import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
 import { fetchNotes } from '../../../../lib/api';
 import NotesClient from './NotesClient';
+import { JSX } from 'react/jsx-runtime';
 
-export default async function NotesPage({
+export default (async function NotesPage({
   params,
 }: {
   params: { slug: string[] };
 }) {
-  const { slug } = params;
-
   const queryClient = new QueryClient();
-
-  const rawTag = slug?.[0] || '';
+  const rawTag = params.slug?.[0] || '';
   const tag = rawTag.toLowerCase() === 'all' ? '' : rawTag;
-
   const data = await fetchNotes(1, 12, '', tag);
-
   await queryClient.prefetchQuery({
     queryKey: ['notes', 1, '', tag],
     queryFn: () => Promise.resolve(data),
   });
-
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
       <NotesClient initialData={data} tag={tag} />
     </HydrationBoundary>
   );
-}
+}) as unknown as (props: { params: { slug: string[] } }) => Promise<JSX.Element>;
